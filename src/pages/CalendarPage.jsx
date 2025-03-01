c8a8a44208134334bdbdf3b759c44e640500d40c
Etern1ty666 <novikov-reg@mail.ru>
2025-03-01 14:03:16 +0300

    calendar update

--- src/pages/CalendarPage.jsx
+++ src/pages/CalendarPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useCallback } from "react";
 import { Calendar, Space } from "antd";
 import PageHeader from "../components/PageHeader";
 import { subscriptionHavePayment } from "../utils/DateFunctions";
@@ -8,46 +8,43 @@ import subscriptionsStore from "../store/subscriptionsStore";
 import Loading from "../components/Loading";
 import dayjs from "dayjs"
 
-
 const CalendarPage = () => {
     const [selectedDate, setSelectedDate] = useState(dayjs())
 
-    const renderDate = (date) => {
-        const dateSubscriptions = []
-    
-        subscriptionsStore.data.map(subscription => {
-            if(subscriptionHavePayment(subscription.paymentDate, subscription.period, subscription.periodType, date)){
-                dateSubscriptions.push(subscription)
-            }
-        })
-        
-    return <Space size='small' align='center' style={{height: 40}}> 
-                    {
-                        dateSubscriptions.length > 0 && selectedDate.month() == date.month()
-                        ?
-                        <CalendarList text={date.date()} data={dateSubscriptions} date={date}/>
-                        :
-                        date.date()
-                    }
+    const renderDate = useCallback((date) => {
+        console.log('here')
+        const dateSubscriptions = subscriptionsStore.data.filter(subscription => 
+            subscriptionHavePayment(subscription.paymentDate, subscription.period, subscription.periodType, date)
+        );
+
+        if (dateSubscriptions.length > 0 && selectedDate.month() === date.month()) {
+            return (
+                <Space size="small" align="center" style={{ height: 40 }}>
+                    <CalendarList text={date.date()} data={dateSubscriptions} date={date} />
                 </Space>
-    }
-    
+            );
+        }
+        
+        return <Space size="small" align="center" style={{ height: 40 }}>{date.date()}</Space>;
+    }, [selectedDate]);
 
     return (
         <>
-            <PageHeader PageName={'Календарь платежей'}/>
-            {subscriptionsStore.isLoading
-                ?
-                <Loading/>
-                :
-                <>
-                    <div style={{margin: 20}}>
-                        <Calendar value={selectedDate} onChange={setSelectedDate} fullscreen={false} fullCellRender={renderDate} />
-                    </div>
-                </>
-            }
-            
+            <PageHeader PageName={'Календарь платежей'} />
+            {subscriptionsStore.isLoading ? (
+                <Loading />
+            ) : (
+                <div style={{ margin: 20 }}>
+                    <Calendar 
+                        value={selectedDate} 
+                        onChange={setSelectedDate} 
+                        fullscreen={false} 
+                        fullCellRender={renderDate} 
+                    />
+                </div>
+            )}
         </>
-)};
-  
+    );
+};
+
 export default observer(CalendarPage);
