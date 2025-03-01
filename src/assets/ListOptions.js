3270045480ebc6eefb8d4a5f45e9e32458e74646
Etern1ty666 <novikov-reg@mail.ru>
2025-03-01 17:00:36 +0300

    upd statistics

--- src/assets/ListOptions.js
+++ src/assets/ListOptions.js
@@ -54,61 +54,74 @@ export const categories = [
   {
       value: 'media',
       label: '📺 Развлечения и медиа',
-      color: '#8021ac'
+      color: '#8021ac',
+      id: 1,
+
   },
   {
       value: 'internet',
       label: '📡 Связь и интернет',
-      color: '#337ca7'
+      color: '#337ca7',
+      id: 2,
   },
   {
       value: 'soft',
       label: '💻 Софт и облачные сервисы',
-      color: '#21aca7'
+      color: '#21aca7',
+      id: 3,
   },
   {
       value: 'health',
       label: '💪 Здоровье и спорт',
-      color: '#3dd129'
+      color: '#3dd129',
+      id: 4,
   },
   {
       value: 'auto',
       label: '🚗 Авто и транспорт',
-      color: '#a70909'
+      color: '#a70909',
+      id: 5,
   },
   {
       value: 'food',
       label: '🍔 Еда и доставка',
-      color: '#d2bc15'
+      color: '#d2bc15',
+      id: 6,
   },
   {
       value: 'games',
       label: '🎮 Игры и развлечения',
-      color: '#bc681e'
+      color: '#bc681e',
+      id: 7,
   },
   {
       value: 'study',
       label: '📚 Образование и работа',
-      color: '#451ebc'
+      color: '#451ebc',
+      id: 8,
   },
   {
       value: 'house',
       label: '🏡 Дом и коммуналка',
-      color: '#1ebc4d'
+      color: '#1ebc4d',
+      id: 9,
   },
   {
       value: 'finances',
       label: '💰 Финансы и инвестиции',
-      color: '#0b5618'
+      color: '#0b5618',
+      id: 10,
   },
   {
       value: 'other',
       label: '🔹 Другое',
-      color: '#1717a6'
+      color: '#1717a6',
+      id: 11,
   },
   {
       value: 'nocategory',
       label: '➖ Без категории',
-      color: '#5e5e5e'
+      color: '#5e5e5e',
+      id: 12,
   },
 ]
--- src/pages/StatisticsPage.jsx
+++ src/pages/StatisticsPage.jsx
@@ -1,5 +1,5 @@
 import { observer } from 'mobx-react-lite';
-import { Card, Flex, Progress, Space, Statistic, Typography } from "antd";
+import { Card, Divider, Flex, Progress, Space, Statistic, Typography } from "antd";
 import PageHeader from '../components/PageHeader';
 
 import { useEffect, useState } from 'react';
@@ -9,24 +9,27 @@ import subscriptionsStore from '../store/subscriptionsStore';
 import { getClearCost } from '../utils/Formatting';
 
 function StatisticsPage() {
-    const categoryMap = Object.fromEntries(categories.map(cat => [cat.value, {label: cat.label, color: cat.color}]));
+    const categoryMap = Object.fromEntries(categories.map(cat => [cat.value, {label: cat.label, color: cat.color, id: cat.id}]));
 
+    var iterations = 0;
     const categoryData = subscriptionsStore.data.reduce((acc, item) => {
         const categoryInfo = categoryMap[item.category];
         if (!acc[categoryInfo.label]) {
-            acc[categoryInfo.label] = { category: categoryInfo.label, items: [], count: 0, color: categoryInfo.color};
+            acc[categoryInfo.label] = { category: categoryInfo.label, items: [], count: 0, color: categoryInfo.color, id: iterations };
         }
 
         acc[categoryInfo.label].items.push(item);
         acc[categoryInfo.label].count++;
+        iterations++;
         return acc;
     }, {});
 
     const totalItems = subscriptionsStore.data.length;
 
     const result = Object.values(categoryData)
-    .map(cat => {let percent = ((cat.count / totalItems) * 100);
+    .map((cat) => {let percent = ((cat.count / totalItems) * 100);
         return {
+        id: cat.id,
         label: cat.category,
         value: percent%1===0?percent:parseFloat(percent.toFixed(2)),
         items: cat.items,
@@ -56,12 +59,9 @@ function StatisticsPage() {
         };
 
     }
-
     const [expenses, setExpenses] = useState({perDay: 0, perMonth: 0, perYear: 0})
         
     useEffect(() => {
-        console.log('useEffect')
-        console.log(subscriptionsStore.data)
         const totalExpenses = subscriptionsStore.data.reduce((acc, subscription) => {
             const subExpenses = renderExspenses(subscription.cost, subscription.period, subscription.periodType);
             return {
@@ -74,6 +74,8 @@ function StatisticsPage() {
         setExpenses(totalExpenses);
     }, [subscriptionsStore.data]);
 
+    const [selectedIndex, setSelectedIndex] = useState(null);
+    
     return (
         <>
             <PageHeader PageName={'Статистика'}/>
@@ -92,27 +94,36 @@ function StatisticsPage() {
                     </Flex> 
                 </Card>
                 <Card title='Категории'>
-                    <Flex justify="space-around" >
-                        <PieChart
-                            slotProps={{ legend: { hidden: true } }}
-                            width={200}
-                            height={200}
-                            margin={{top: 0, right: 0, bottom: 0, left: 0}}
-                            tooltip={{disablePortal: true}}
-                            series={[
-                                {
+                    {
+                        subscriptionsStore.data.length===0 && subscriptionsStore.isLoading===false
+                        ?
+                        <Flex justify="space-around" >
+                            <Typography>Пока нет подписок</Typography>
+                        </Flex>
+                        :
+                        <Flex justify="space-around" >
+                            <PieChart
+                                slotProps={{ legend: { hidden: true } }}
+                                width={200}
+                                height={200}
+                                onHighlightChange={(value) => { if(value!==null){setSelectedIndex(value.dataIndex)}}}
+                                onItemClick={(event, d) => { if(d!==null){setSelectedIndex(d.dataIndex)}}}
+                                margin={{ top: 0, right: 0, bottom: 0, left: 0 }}
+                                tooltip={{ disablePortal: true }}
+                                series={[
+                                    {
                                     data: result,
                                     innerRadius: 60,
                                     outerRadius: 90,
-                                }
-                            ]}
-                        />
-                    </Flex>
-                    {result. map(item => {
+                                    }
+                                ]}
+                            />
+                        </Flex>
+                    }
+                    
+                    {result.map(item => {
                         return <>
                             <Typography>{item.label} {item.value}%</Typography>
-                            
-
                             <Progress strokeColor={item.color} percent={item.value} status="active" showInfo={false}/>
                         </>
                     })}
